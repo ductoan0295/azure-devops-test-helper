56a91b33641d261b8a4979c322cd4a1e008df473
Toan <ductoan0295@gmail.com>
2023-04-06 18:00:56 +0700

    implement reports validation mechanism before importing process using json schema, add exported method to import directly from the provided directory, add npmignore file

--- src/importer.ts
+++ src/importer.ts
@@ -8,48 +8,95 @@ import {
   TestPoint,
   TestRun,
 } from "azure-devops-node-api/interfaces/TestInterfaces.js";
+import path from "node:path";
+import url from "node:url";
+import fs from "node:fs";
+import Ajv, { Schema } from "ajv";
+import addFormats from "ajv-formats";
 
 class AzureDevopsResultImporter {
   private azureTestApiClient!: ITestApi;
+  private ajv: Ajv;
+  private testReportSchema: Schema;
+
+  constructor() {
+    this.ajv = new Ajv();
+    addFormats(this.ajv);
+    this.testReportSchema = JSON.parse(
+      fs
+        .readFileSync(
+          path.join(url.fileURLToPath(new URL(".", import.meta.url)), "./schema/TestReportSchema.json")
+        )
+        .toString()
+    );
+  }
+
+  public async importReportFilesToTestRun(
+    reportAbsoluteDir: string,
+    config: AzureResultImporterConfig
+  ): Promise<TestCaseResult[]> {
+    if (!fs.existsSync(reportAbsoluteDir) || !path.isAbsolute(reportAbsoluteDir)) {
+      throw new Error("Report directory does not exist or is not an absolute path!!!");
+    }
+    const reportFiles = fs
+      .readdirSync(reportAbsoluteDir)
+      .filter((file: string) => path.extname(file) === ".json");
+
+    const reports: TestReport[] = [];
+    reportFiles.forEach((file: string) => {
+      const fileData = fs.readFileSync(path.join(reportAbsoluteDir, file));
+      reports.push(JSON.parse(fileData.toString()));
+    });
+
+    const importedTestResults: TestCaseResult[] = await this.importTestResultToTestRun(reports, config);
+    return importedTestResults;
+  }
 
   public async importTestResultToTestRun(
     testReports: TestReport[],
     config: AzureResultImporterConfig
   ): Promise<TestCaseResult[]> {
-    await this.createAzureTestAPIClient(config.pat, config.organizationUrl);
-
-    const executedConfigurationIds: number[] = testReports.map((report: TestReport) =>
-      Number(report.azureConfigurationId)
+    const validFormatReports = testReports.filter((report: TestReport) =>
+      this.ajv.validate(this.testReportSchema, report)
     );
 
-    const testRun = await this.createTestRun(this.azureTestApiClient, config, executedConfigurationIds);
-    if (!testRun.id) {
-      throw new Error("Failed to create test Run!");
-    }
-    await this.setRunStatus(this.azureTestApiClient, config, testRun.id, AzureTestRunStatus.INPROGRESS);
+    if (validFormatReports.length > 0) {
+      await this.createAzureTestAPIClient(config.pat, config.organizationUrl);
 
-    const updatingResults: TestCaseResult[] = await this.getUpdatingTestResult(
-      testReports,
-      config.project,
-      testRun.id
-    );
+      const executedConfigurationIds: number[] = validFormatReports.map((report: TestReport) =>
+        Number(report.azureConfigurationId)
+      );
 
-    let importedTestResults: TestCaseResult[] = [];
-    try {
-      importedTestResults = await this.azureTestApiClient.updateTestResults(
-        updatingResults,
+      const testRun = await this.createTestRun(this.azureTestApiClient, config, executedConfigurationIds);
+      if (!testRun.id) {
+        throw new Error("Failed to create test Run!");
+      }
+      await this.setRunStatus(this.azureTestApiClient, config, testRun.id, AzureTestRunStatus.INPROGRESS);
+
+      const updatingResults: TestCaseResult[] = await this.getUpdatingTestResult(
+        validFormatReports,
         config.project,
         testRun.id
       );
-    } catch (error: unknown) {
-      await this.setRunStatus(this.azureTestApiClient, config, testRun.id, AzureTestRunStatus.ABORTED);
-      throw new Error(
-        "Failed to import result into the created Test Run! with the following error: \n" + error
-      );
-    }
 
-    await this.setRunStatus(this.azureTestApiClient, config, testRun.id, AzureTestRunStatus.COMPLETED);
-    return importedTestResults;
+      let importedTestResults: TestCaseResult[] = [];
+      try {
+        importedTestResults = await this.azureTestApiClient.updateTestResults(
+          updatingResults,
+          config.project,
+          testRun.id
+        );
+      } catch (error: unknown) {
+        await this.setRunStatus(this.azureTestApiClient, config, testRun.id, AzureTestRunStatus.ABORTED);
+        throw new Error(
+          "Failed to import result into the created Test Run! with the following error: \n" + error
+        );
+      }
+
+      await this.setRunStatus(this.azureTestApiClient, config, testRun.id, AzureTestRunStatus.COMPLETED);
+      return importedTestResults;
+    }
+    return [];
   }
 
   private async getUpdatingTestResult(
